353364da2603d40e2f03840f9aa779b03d888b8c
Saim Ali Nadeem <saimalinadeem4@gmail.com>
2023-05-03 14:25:47 +0500

    Transactions in testing /list

--- server/test/listTest.js
+++ server/test/listTest.js
@@ -20,7 +20,14 @@ describe("/list", () => {
     INSERT INTO custom_list (title, users_id)
     VALUES ('test_list', 1) 
     RETURNING *`;
-    const deleteQuery = `DELETE FROM custom_list`;
+
+    // Using transactions as hooks to isolate each test 
+    beforeEach( async() =>{
+        await pool.query("BEGIN");  
+    })
+    afterEach( async() =>{
+        await pool.query("ROLLBACK");  
+    })
 
     // GET /list
     describe("GET /list", () => {
@@ -53,8 +60,6 @@ describe("/list", () => {
 
         it("Should NOT GET any list", async () =>{
 
-            await pool.query(deleteQuery);
-
             const res = await chai.request(app).get("/list");
 
             res.should.have.status(200);
@@ -77,69 +82,70 @@ describe("/list", () => {
 
         });
     });
+
+
+    // POST /list
+    describe("POST /list", () => {
+
+        /* Cases:
+            // Route Success
+            - 201 status code when a list is successfully created
+            - In Success case route responds with list object
+        
+            // Route Failure:
+            - 400 status code when the list creation is unsuccessful
+            - 400 status code when the title is empty
+        
+            // Server Failure
+            - 500 status code when the try/catch block catches any error
+        */
+
+        const listObject = { title: "test post title" };
+        const emptyListObject = { title: "" };
+    
+        it("Should POST a new list", async() => {
+
+            const res = await chai.request(app).post("/list").send(listObject);
+
+            res.should.have.status(201);
+            res.body.should.have.a('object');
+            res.body.data.should.have.property('title').eq("test post title");
+        });
+    
+        // it("Should NOT POST a new list", async() => {
+
+        //     // await pool.query(deleteQuery);
+    
+        //     chai.request(app)
+        //         .post("/list")
+        //         .send(listObject)
+        //         .end((err, res) => {
+        //             res.should.have.status(400);
+        //             done();
+        //         });
+        // });
+    
+        // it("Should return with 500 status code", (done) => { /* research */
+        //     const listObject = { title: '500 test' };
+    
+        //     const queryError = new Error('Database error');
+        //     sinon.stub(pool, 'query').throws(queryError);
+    
+        //     chai.request(app)
+        //         .post('/list')
+        //         .send(listObject)
+        //         .end((err, res) => {
+        //             res.should.have.status(500);
+        //             res.text.should.equal('An error occurred, please try again later.');
+        //             sinon.restore();
+        //             done();
+        //         });
+        // });
+    
+    });
+    
 });
 
-
-// POST /list
-// describe("POST /list", () => {
-
-//     /* Cases:
-//         // Route Success
-//         - 201 status code when a list is successfully created
-//         - In Success case route responds with list object
-    
-//         // Route Failure:
-//         - 400 status code when the list creation is unsuccessful
-//         - 400 status code when the title is empty
-    
-//         // Server Failure
-//         - 500 status code when the try/catch block catches any error
-//     */
-
-//     it("Should POST a new list", (done) => {
-//         const listObject = { title: "201 test" };
-
-//         chai.request(app) /* research about why is this "app" here */
-//             .post("/list")
-//             .send(listObject)
-//             .end((err, res) => {
-//                 res.should.have.status(201);
-//                 res.body.should.have.a('object');
-//                 res.body.data.should.have.property('title').eq("201 test");
-//                 done();
-//             });
-//     });
-
-//     it("Should NOT POST a new list", (done) => {
-//         const listObject = { title: "" };
-
-//         chai.request(app)
-//             .post("/list")
-//             .send(listObject)
-//             .end((err, res) => {
-//                 res.should.have.status(400);
-//                 done();
-//             });
-//     });
-
-//     it("Should return with 500 status code", (done) => { /* research */
-//         const listObject = { title: '500 test' };
-
-//         const queryError = new Error('Database error');
-//         sinon.stub(pool, 'query').throws(queryError);
-
-//         chai.request(app)
-//             .post('/list')
-//             .send(listObject)
-//             .end((err, res) => {
-//                 res.should.have.status(500);
-//                 res.text.should.equal('An error occurred, please try again later.');
-//                 sinon.restore();
-//                 done();
-//             });
-//     });
-
-// });
 
 // PATCH /list
 // describe("PATCH /list", () => {
